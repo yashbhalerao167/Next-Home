e1ec15e8b1d8400f5ea251e721a7dc9aa89b0f97
yashbhalerao167 <yashbhalerao167@gmail.com>
2026-02-15 18:40:36 +0530

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,4 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/request';
+import { NextResponse, type NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname;
